8b48e084b28b1b670db56ce113b105d0a72c4a4e
jobunityARproject <69096744+jobunityARproject@users.noreply.github.com>
2022-12-20 16:57:52 +0700

    อัพโหลดกระดองเต่าวาดสี่เหลี่ยมห้ารูป

---
+++ KadongTao.py
@@ -0,0 +1,92 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+tao = turtle.Pen()
+tao.shape('turtle')
+def Rectangle():
+    for i in range(4):
+        tao.forward(100)
+        tao.left(90)
+
+        
+Rectangle()
+tao.clear()
+def Go(x,y):
+    tao.penup()
+    tao.goto(x,y)
+    tao.pendown()
+
+    
+Go(200,200)
+Go(-200,100)
+Rectangle()
+Go(-200,-200)
+Rectangle()
+Go(0,0)
+Rectangle()
+Go(200,200)
+Rectangle()
+Go(200, -200)
+Rectangle()
+tao.clear()
+Go(0,0)
+for i in range(5)
+SyntaxError: expected ':'
+for i in range(5):
+    Rectangle()
+    Go(-200, 200)
+
+    
+tao.clear()
+Go(0,0)
+for i in range(5):
+    tao.circle(50)
+    tao.left(36)
+
+    
+for i in range(5):
+    tao.circle(50)
+    tao.left(36)
+
+    
+tao.clear()
+for i in range(5):
+    Rectangle()
+    Go(-200,200)
+    Rectangle()
+    Go(-200,-200)
+    Rectangle()
+    Go(200,-200))
+    
+SyntaxError: unmatched ')'
+for i in range(5):
+    Rectangle()
+    Go(-200,200)
+    Rectangle()
+    Go(-200,-200)
+    Rectangle()
+    Go(200,-200)
+    Rectangle()
+    Go(200,200)
+    Rectangle()
+
+    
+cltao.clear()
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    cltao.clear()
+NameError: name 'cltao' is not defined
+tao.clear()
+Go(0,0)
+for i in range(1):
+    Rectangle()
+    Go(-200,200)
+    Rectangle()
+    Go(-200,-200)
+    Rectangle()
+    Go(200,-200)
+    Rectangle()
+    Go(200,200)
+    Rectangle()
+    Go(0,0)
+
